279138d8103d9ff7f76c83dc6b8cbf36195f6dba
Srinivasa Rao M <147464732+srinimsr007@users.noreply.github.com>
2024-07-13 16:15:21 +0530

    Create SumEvenOdd.java

---
+++ SumEvenOdd.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumEvenOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+
+        if (sum % 2 == 0) {
+            System.out.println("The sum is EVEN.");
+        } else {
+            System.out.println("The sum is ODD.");
+        }
+    }
+}
