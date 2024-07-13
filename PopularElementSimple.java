336ee86ad6ae9d1043806f3bdb46a8606854f575
Srinivasa Rao M <147464732+srinimsr007@users.noreply.github.com>
2024-07-13 16:19:27 +0530

    Create PopularElementSimple.java

---
+++ PopularElementSimple.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class PopularElementSimple {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        
+        int threshold = n / 4;
+        int popularElement = -1;
+
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+            if (count > threshold) {
+                popularElement = arr[i];
+                break;
+            }
+        }
+
+        System.out.println(popularElement);
+    }
+}
